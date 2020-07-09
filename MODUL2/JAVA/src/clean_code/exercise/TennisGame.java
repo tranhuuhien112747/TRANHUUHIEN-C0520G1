03fa768f420edf84583e86470e6a4adb4169df9b
tranhuuhien112747 <tranhuuhien112747@gmail.com>
2020-07-09 09:56:04 +0700

    :cap nhat bt

--- MODUL2/JAVA/src/clean_code/exercise/TennisGame.java
+++ MODUL2/JAVA/src/clean_code/exercise/TennisGame.java
@@ -23,32 +23,35 @@ public class TennisGame {
                     break;
 
             }
-        } else if (firstPlayerScore >= 4 || secondPlayerScore >= 4) {
-            int minusResult = firstPlayerScore - secondPlayerScore;
-            if (minusResult == 1) displayScore  = "Advantage player1";
-            else if (minusResult == -1) displayScore  = "Advantage player2";
-            else if (minusResult >= 2) displayScore  = "Win for player1";
-            else displayScore  = "Win for player2";
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = firstPlayerScore;
-                else {
-                    displayScore  += "-";
-                    tempScore = secondPlayerScore;
-                }
-                switch (tempScore) {
-                    case 0:
-                        displayScore  += "Love";
-                        break;
-                    case 1:
-                        displayScore  += "Fifteen";
-                        break;
-                    case 2:
-                        displayScore  += "Thirty";
-                        break;
-                    case 3:
-                        displayScore  += "Forty";
-                        break;
+            boolean scoreOverFour = firstPlayerScore >= 4 || secondPlayerScore >= 4;
+            if (scoreOverFour) {
+                int minusResult = firstPlayerScore - secondPlayerScore;
+                if (minusResult == 1) displayScore  = "Advantage player1";
+                else if (minusResult == -1) displayScore  = "Advantage player2";
+                else if (minusResult >= 2) displayScore  = "Win for player1";
+                else displayScore  = "Win for player2";
+            } else {
+                for (int i = 1; i < 3; i++) {
+                    if (i == 1) tempScore = firstPlayerScore;
+                    else {
+                        displayScore  += "-";
+                        tempScore = secondPlayerScore;
+                    }
+                    switch (tempScore) {
+                        case 0:
+                            displayScore  += "Love";
+                            break;
+                        case 1:
+                            displayScore  += "Fifteen";
+                            break;
+                        case 2:
+                            displayScore  += "Thirty";
+                            break;
+                        case 3:
+                            displayScore  += "Forty";
+                            break;
+                    }
                 }
             }
         }
