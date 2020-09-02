420165780325791f4498eab49953b7c871818b2b
tranhuuhien112747 <tranhuuhien112747@gmail.com>
2020-09-02 19:54:57 +0700

    :cap nhat bai tap

---
+++ MODULE3/Case_Study2/Severlet/web/view/view.jsp
@@ -0,0 +1,78 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Tran Huu Hien
+  Date: 02/09/2020
+  Time: 2:57 CH
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>MAIN</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<link rel="stylesheet" type="text/css" href="/bootstrap_4/bootstrap-4.5.2-dist/css/bootstrap.min.css">
+<script src="/bootstrap_4/bootstrap-4.5.2-dist/js/bootstrap.min.js"></script>
+<script src="/bootstrap_4/bootstrap-4.5.2-dist/js/jquery-3.5.1.js"></script>
+<body>
+<header class="row" style="height: 70px">
+    <img class="col-xl-3" src="/imagi/logo.jpg" alt="picture"
+         style="height: 60px;text-align: center;padding-top: 5px ">
+    <div class="col-xl-9" style=" padding-left: 700px ;align-content: center; text-align: center;padding-top: 5px ">
+        <h4 style="color: red ;font-weight: bolder">TRAN HUU HIEN</h4>
+        <h4>Class: C0520G1 </h4>
+    </div>
+</header>
+<nav class="navbar navbar-expand-lg navbar-darkblue"
+     style="background-color: #e3f2fd;border: 2px solid darkblue;height: 80px;padding-left: 20px">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="font-weight: bolder;font-size: 20px">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Employee</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Customer</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Service</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Contract</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0" style="padding-right: 20px">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="font-weight: bolder">Search
+            </button>
+        </form>
+    </div>
+</nav>
+<div class="container-fluid">
+    <div class="row" style="height: 400px">
+        <div class="col-xl-2" style="border: 1px solid darkblue">
+            <div>
+                <a href="#">Item one</a>
+            </div>
+
+            <div>
+                <a href="#">Item two</a>
+            </div>
+            <div>
+                <a href="#">Item three</a>
+            </div>
+        </div>
+        <div class="col-xl-10" style="border: 1px solid darkblue;text-align: center">
+            <h1>Body</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xl-12" style="border: 1px solid darkblue; text-align: center">
+            <h1>FOOTER</h1>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ MODULE3/Case_Study2/sql/case_study2.sql
@@ -0,0 +1,130 @@
+drop database if exists module3_furama;
+create database module3_furama;
+use module3_furama;
+
+create table `role` (
+	role_id int primary key,
+	role_name varchar(45) not null
+);
+
+create table `user` (
+	username varchar(45) primary key,
+	`password` varchar(45) not null
+);
+
+create table user_role (
+	role_id int primary key,
+	username varchar(45) not null,
+	foreign key (role_id) references `role` (role_id) on delete cascade,
+	foreign key (username) references `user` (username) on delete cascade
+);
+
+create table position (
+	position_id int primary key,
+	position_name varchar(45) not null
+);
+
+create table division (
+	division_id int primary key,
+	division_name varchar(45) not null
+);	
+
+create table education_degree (
+	education_degree_id int primary key,
+	education_degree_name varchar(45) not null
+);
+
+create table employee (
+	employee_id int primary key,
+	employee_name varchar(45) not null,
+	employee_birthday date not null,
+	employee_id_card varchar(45) not null,
+	employee_salary double not null,
+	employee_phone varchar(45) not null,
+	employee_email varchar(45) not null,
+	employee_adress varchar(45) not null,
+	position_id int not null,
+	division_id int not null,
+	education_degree_id int not null,
+	username varchar(255),
+    foreign key (position_id)references position (position_id) on delete cascade,
+    foreign key (division_id)references division (division_id) on delete cascade,
+    foreign key (education_degree_id)references education_degree (education_degree_id) on delete cascade,
+    foreign key (username)references `user`(username) on delete cascade
+);
+
+create table customer_type (
+	customer_type_id int primary key,
+	customer_type_name varchar(45) not null
+);
+
+create table customer (
+	customer_id int primary key,
+	customer_type_id int not null,
+	customer_name varchar(45) not null,
+	customer_birthday date not null,
+	customer_gender bit(1),
+	customer_id_card varchar(45) not null,
+	customer_phone varchar(45) not null,
+	customer_email varchar(45) not null,
+	customer_address varchar(45) not null,
+	foreign key (customer_type_id) references customer_type (customer_type_id) on delete cascade
+);
+
+create table rent_type (
+	rent_type_id int primary key,
+	rent_type_name varchar(45) not null,
+	rent_type_cost double not null
+);
+
+create table service_type (
+	service_type_id int primary key,
+	service_type_name varchar(45) not null
+);
+
+create table service (
+	service_id int primary key,
+	service_name varchar(45) not null,
+	service_area int not null,
+	service_cost  double not null,
+	service_max_people int not null,
+	rent_type_id int not null,
+	service_type_id int not null,
+	standard_room varchar(45) not null,
+	description_other varchar(45) not null,
+	pool_area double not null,
+	number_floor int not null,
+	foreign key (rent_type_id) references rent_type (rent_type_id) on delete cascade,
+	foreign key (service_type_id) references service_type (service_type_id) on delete cascade
+);
+
+create table attach_service (
+	attach_service_id int primary key,
+	attach_service_name varchar(45) not null,
+	attach_service_cost double not null,
+	attach_service_unit int not null,
+	attach_service_status varchar(45) not null
+);
+
+create table contract (
+	contract_id int primary key,
+	contract_star_date datetime not null,
+	contract_end_date datetime not null,
+	contract_deposit double not null,
+	contract_total_money double not null,
+	employee_id int not null,
+	customer_id int not null,
+	service_id int not null,
+	foreign key (employee_id) references employee (employee_id) on delete cascade,
+	foreign key (customer_id) references customer (customer_id) on delete cascade,
+	foreign key (service_id) references service (service_id) on delete cascade
+);
+
+create table contract_detail (
+	contract_detail_id int not null,
+	contract_id int,
+	attach_service_id int,
+	quantity int not null,
+	foreign key (contract_id) references contract (contract_id) on delete cascade,
+	foreign key (attach_service_id) references attach_service (attach_service_id) on delete cascade
+);
