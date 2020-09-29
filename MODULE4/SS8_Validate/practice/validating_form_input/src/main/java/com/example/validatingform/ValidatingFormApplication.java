bfd39e166627efc0f0d30842485b7036b342bd33
tranhuuhien112747 <tranhuuhien112747@gmail.com>
2020-09-29 11:59:38 +0700

    cap nhat

---
+++ MODULE4/SS8_Validate/practice/validating_form_input/src/main/java/com/example/validatingform/ValidatingFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.validatingform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatingFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidatingFormApplication.class, args);
+    }
+
+}
---
+++ MODULE4/SS8_Validate/practice/validating_form_input/src/test/java/com/example/validatingform/ValidatingFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validatingform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
