8eb75082dcb69cf9fcd3a2bdbe90a1938080219d
tranhuuhien112747 <tranhuuhien112747@gmail.com>
2020-09-29 23:02:42 -0700

    cap nhat

---
+++ MODULE4/SS8_Validate/exercise/create-form/src/main/java/com/code/demo/FromApplication.java
@@ -0,0 +1,13 @@
+package com.code.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FromApplication.class, args);
+    }
+
+}
